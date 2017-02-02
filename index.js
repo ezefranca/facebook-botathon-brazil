3af1cac85da21e022603852369c3e7c4b1415346
ezefranca <ezequiel.ifsp@gmail.com>
2017-02-02 17:37:37 -0200

    update ocr test 8

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.post('/webhook/', function (req, res) {
 				var imageURL = event.message.attachments[0].payload.url;
 				console.log(imageURL);
 				//sendTextMessage(sender, "Me mandou foto aqui ?" + imageURL)
-				//ocrDetector(imageURL)
+				ocrDetector(imageURL)
 			}
 		}
 	}
